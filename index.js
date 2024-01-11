476d3b465c9daca5e306000050e85eeb488209b4
Anna Kioko <annakioko00@gmail.com>
2024-01-11 11:10:50 +0300

    completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(function (tutorial) {
+    return tutorial
+      .split(" ")
+      .map(function (word) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      })
+      .join(" ");
+  });
 }
+
+
+const titleCasedTutorials = titleCased();
+console.log(titleCasedTutorials);
